b3576ded02b3caf28baa2df002b2dfa279b8062d
Rafael Kellermann Streit <rafaelkellermann@gmail.com>
2011-10-13 22:10:01 -0300

    Added support to generic fields maks

--- README.markdown
+++ README.markdown
@@ -1,7 +1,7 @@
 <h1>What?</h1>
-<p>This "Class", help you to make maskfields in your textFields of Titanium. For example: The postcode 99999999, will be 99999-999.</p>
+<p>This "Class", will help you to make maskfields in your textFields of Titanium. For example: The postcode 99999999, will be 99999-999.</p>
 <h1>How?</h1>
-<p>It's very sample... You just have to use this function, in the "change" event of textField.</p>
+<p>It's very easy... You just have to use this function, in the "change" textField event.</p>
 <p>Example:</p>
 <pre>
 Ti.include("mask.js");
@@ -16,5 +16,16 @@ var textfield = Ti.UI.createTextField({
 
 textfield.addEventListener("change", function() {
 	Mask.mask(textfield, Mask.postcode);
+});
+</pre>
+<h1>Do you need a generic mask in your field?</h1>
+<p>You will need to send three parameters in the function: regex (required), sintax (required) and maxValue (optional). See the example:</p>
+<pre>
+textFieldPostcode.addEventListener("change", function(evt) {
+	Mask.mask(textFieldPostcode, Mask.generic, {
+		regex: /^(\d{5})(\d)/,
+		sintax: "$1-$2",
+		maxValue: 9
+	});
 });
 </pre>
--- mask.js
+++ mask.js
@@ -5,20 +5,33 @@
 
 Mask = 
 {
-	mask: function(_field, _function) {
-		_field.value = _function(_field.value)
+	mask: function(_field, _function, _genericParameters) {
+		if (_genericParameters)
+			_field.value = _function(_field.value, _genericParameters);
+		else
+			_field.value = _function(_field.value);
 	},
 	
+	generic: function(v, _genericParameters) {
+		var _regex = _genericParameters.regex;
+		var _sintax = _genericParameters.sintax;
+		var _maxValue = _genericParameters.maxValue;
+		v = v.replace(/D/g,"");
+		v = v.replace(_regex, _sintax);
+		
+		return (_maxValue != null) ? v.slice(0, _maxValue) : v;
+	},
+
 	postcode: function(v) {
-	    v = v.replace(/D/g,"");
-	    v = v.replace(/^(\d{5})(\d)/,"$1-$2");
-	    return v.slice(0, 9);
+		v = v.replace(/D/g,"");
+		v = v.replace(/^(\d{5})(\d)/,"$1-$2");
+		return v.slice(0, 9);
 	},
 	
 	phone: function(v) {
-	    v = v.replace(/\D/g,"");
-	    v = v.replace(/^(\d\d)(\d)/g,"($1) $2");
-	    v = v.replace(/(\d{4})(\d)/,"$1-$2");
-	    return v.slice(0, 14);
+		v = v.replace(/\D/g,"");
+		v = v.replace(/^(\d\d)(\d)/g,"($1) $2");
+		v = v.replace(/(\d{4})(\d)/,"$1-$2");
+		return v.slice(0, 14);
 	}
 }
